ae431bfd541b78f808e37ed004f6c8b4617714f8
Sadman Shahriar <118925483+SadmanShahriar@users.noreply.github.com>
2023-03-23 04:31:11 +0600

    Add files via upload

---
+++ dartfunction/bin/dartfunction.dart
@@ -0,0 +1,35 @@
+// ignore: unused_import
+import 'package:dartfunction/dartfunction.dart' as dartfunction;
+import 'package:test/test.dart';
+
+void main() {
+
+//arrow function
+ arrowFun()=> print(20+20);
+  arrowFun();
+
+
+// optional positional parameter
+  myfunc(a, b, [c]) {
+     print(a);
+     print(b);
+     print(c);
+}
+ myfunc(10,5); 
+
+ // optional name parameter
+    myFunc(a, b, {c}) {
+     print(a);
+     print(b);
+     print(c);
+}
+ myFunc(10,5); 
+
+// 
+
+
+
+ 
+
+
+}
