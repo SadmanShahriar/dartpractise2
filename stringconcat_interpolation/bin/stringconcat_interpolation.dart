f9fd1bfdcad88498a04016549e869f940c6a781e
Sadman Shahriar <118925483+SadmanShahriar@users.noreply.github.com>
2023-03-23 04:31:28 +0600

    Add files via upload

---
+++ stringconcat_interpolation/bin/stringconcat_interpolation.dart
@@ -0,0 +1,23 @@
+
+
+void main() {
+ String a = 'we';
+ String b = 'are';
+ String c = 'learning';
+ String d = 'Dart';
+
+//1st step
+ print('$a$b$c$d');
+
+ //2nd step
+ print(a+b+c+d);
+ 
+
+ //string literal
+  String e = 'we' 'are' 'learning''dart';
+  print(e);
+
+//4th step
+  List <String> f = ['we', 'are', 'learning', 'dart'];
+  print(f.join());
+}
---
+++ stringconcat_interpolation/test/stringconcat_interpolation_test.dart
@@ -0,0 +1,8 @@
+import 'package:stringconcat_interpolation/stringconcat_interpolation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
